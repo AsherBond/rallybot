b20e7e4d3fd63889b4150ac8074bc6100fccee02
galstrom21 <jshepher@rackspace.com>
2013-05-14 12:31:14 -0500

    Adding kill session

--- run_in_screen.sh
+++ run_in_screen.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "Killing rallybot screen session"
+screen -S rallybot -X stuff 
+
 echo "Starting rallybot in a screen session"
 screen -S rallybot -t rallybot -d -m ./rallybot.rb
