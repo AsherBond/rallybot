b994be09982faca04fab5d1be51b3dc43432f80c
galstrom21 <jshepher@rackspace.com>
2013-05-14 12:14:34 -0500

    fixing setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# This gem has native C extensions
+echo "Installing nokogiri"
+gem install nokogiri --no-ri --no-rdoc
+
 echo "Unpacking ruby gems"
 mkdir -p vendor/gems/
-gems=( cinch rally_api html2md )
+
+gems=( cinch rally_api html2md httpclient nokogiri )
 for g in ${gems[@]}; do
     gem unpack $g --target=./vendor/gems/
 done
